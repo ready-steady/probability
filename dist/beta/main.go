aa74a8257716fc30b09fad4e3370c61fc90beb96
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-09-23 06:20:37 +0200

    The first version of beta.InvCDF

--- dist/beta/main.go
+++ dist/beta/main.go
@@ -27,19 +27,32 @@ func (s *Self) CDF(points []float64) []float64 {
 	α, β, k, b := s.α, s.β, s.b-s.a, s.a
 
 	for i, x := range points {
-		values[i] = betaInc(α, β, (x-b)/k)
+		values[i] = ibeta(α, β, (x-b)/k)
 	}
 
 	return values
 }
 
-func betaInc(α, β, x float64) float64 {
+// InvCDF evaluates the inverse CDF of the distribution.
+func (s *Self) InvCDF(points []float64) []float64 {
+	values := make([]float64, len(points))
+
+	α, β, k, b := s.α, s.β, s.b-s.a, s.a
+
+	for i, p := range points {
+		values[i] = k*invbeta(p, α, β) + b
+	}
+
+	return values
+}
+
+func ibeta(α, β, x float64) float64 {
 	// Author: John Burkardt
 	// Source: http://people.sc.fsu.edu/~jburkardt/c_src/prob/prob.html
 
 	const (
 		max = 1000
-		tol = 1.0e-07
+		tol = 1e-07
 	)
 
 	if x <= 0 {
@@ -111,4 +124,153 @@ func beta(x, y float64) float64 {
 	y, _ = math.Lgamma(y)
 
 	return math.Exp(x + y - z)
+}
+
+func invbeta(cdf, p, q float64) float64 {
+	const (
+		sae = -37
+	)
+
+	var flip bool
+	var iex int
+	var a, acu, adj, beta_log, fpu, g, h float64
+	var pp, prev, qq, r, s, sq, t, tx, value, w, xin, y, yprev float64
+
+	fpu = math.Pow10(sae)
+
+	if cdf == 0 {
+		return 0
+	}
+	if cdf == 1 {
+		return 1
+	}
+
+	if 0.5 < cdf {
+		a = 1 - cdf
+		pp = q
+		qq = p
+		flip = true
+	} else {
+		a = cdf
+		pp = p
+		qq = q
+	}
+
+	// Calculate the initial approximation.
+	r = math.Sqrt(-math.Log(a * a))
+
+	y = r - (2.30753+0.27061*r)/(1+(0.99229+0.04481*r)*r)
+
+	if 1 < pp && 1 < qq {
+		r = (y*y - 3) / 6
+		s = 1 / (pp + pp - 1)
+		t = 1 / (qq + qq - 1)
+		h = 2 / (s + t)
+		w = y*math.Sqrt(h+r)/h - (t-s)*(r+5/6-2/(3*h))
+		value = pp / (pp + qq*math.Exp(w+w))
+	} else {
+		r = qq + qq
+		t = 1 / (9 * qq)
+		t = r * math.Pow(1-t+y*math.Sqrt(t), 3)
+
+		if t <= 0 {
+			value = 1 - math.Exp((math.Log((1-a)*qq)+beta_log)/qq)
+		} else {
+			t = (4*pp + r - 2) / t
+
+			if t <= 1 {
+				value = math.Exp((math.Log(a*pp) + beta_log) / pp)
+			} else {
+				value = 1 - 2/(t+1)
+			}
+		}
+	}
+
+	// A modified Newton–Raphson method
+	r = 1 - pp
+	t = 1 - qq
+	yprev = 0
+	sq = 1
+	prev = 1
+
+	if value < 0.0001 {
+		value = 0.0001
+	}
+
+	if 0.9999 < value {
+		value = 0.9999
+	}
+
+	iex = int(-5/pp/pp - 1/math.Pow(a, 0.2) - 13)
+	if sae > iex {
+		iex = sae
+	}
+
+	acu = math.Pow10(iex)
+
+	for {
+		y = ibeta(pp, qq, value)
+
+		xin = value
+		y = (y - a) * math.Exp(beta_log+r*math.Log(xin)+t*math.Log(1-xin))
+
+		if y*yprev <= 0 {
+			if sq > fpu {
+				prev = sq
+			} else {
+				prev = fpu
+			}
+		}
+
+		g = 1
+
+		for {
+			for {
+				adj = g * y
+				sq = adj * adj
+
+				if sq < prev {
+					tx = value - adj
+
+					if 0 <= tx && tx <= 1 {
+						break
+					}
+				}
+				g = g / 3
+			}
+
+			if prev <= acu {
+				if flip {
+					value = 1 - value
+				}
+				return value
+			}
+
+			if y*y <= acu {
+				if flip {
+					value = 1 - value
+				}
+				return value
+			}
+
+			if tx != 0 && tx != 1 {
+				break
+			}
+
+			g = g / 3
+		}
+
+		if tx == value {
+			break
+		}
+
+		value = tx
+		yprev = y
+	}
+
+	if flip {
+		value = 1 - value
+	}
+
+	return value
 }
