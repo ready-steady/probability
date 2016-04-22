f831f638a5820e13b271fa18adcc413bf126ce9b
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-04-22 10:28:41 +0200

    Complete Uniform

--- uniform.go
+++ uniform.go
@@ -11,6 +11,16 @@ func NewUniform(a, b float64) *Uniform {
 	return &Uniform{a, b}
 }
 
+// Cumulate evaluates the CDF.
+func (self *Uniform) Cumulate(x float64) float64 {
+	return (x - self.a) / (self.b - self.a)
+}
+
+// Decumulate evaluates the inverse of the CDF.
+func (self *Uniform) Decumulate(p float64) float64 {
+	return (self.b-self.a)*p + self.a
+}
+
 // Sample draws a sample.
 func (self *Uniform) Sample(generator Generator) float64 {
 	return (self.b-self.a)*generator.Float64() + self.a
